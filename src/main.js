9187b3f72e4f7eafd1a19b473773644774d3f031
Dev-Muller <pedromullergl11@gmail.com>
2023-02-14 16:18:39 -0300

    resolve requisito 3

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
-// import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
 
+const itens = document.querySelector('.products');
+const produto = async () => {
+  const response = await fetchProductsList('computador');
+  const variable = response.forEach((e) => {
+    const product = createProductElement(e);
+    itens.appendChild(product);
+  });
+  return variable;
+};
+produto();
 document.querySelector('.cep-button').addEventListener('click', searchCep);
